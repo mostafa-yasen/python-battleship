863039f9131991de962fa47b29e814f8bcfe8962
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2021-05-21 01:07:18 +0200

    first commit

---
+++ battleship/api.py
@@ -0,0 +1,20 @@
+from http import HTTPStatus
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+GAME = None
+
+@app.route('/battleship', methods=['POST'])
+def create_battleship_game():
+    return jsonify({}), HTTPStatus.NOT_IMPLEMENTED
+
+
+@app.route('/battleship', methods=['PUT'])
+def shot():
+    return jsonify({}), HTTPStatus.NOT_IMPLEMENTED
+
+
+@app.route('/battleship', methods=['DELETE'])
+def delete_battleship_game():
+    return jsonify({}), HTTPStatus.NOT_IMPLEMENTED
