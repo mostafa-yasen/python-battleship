5e0d9f4ae8a4894d009cd5d02224a9380e609a50
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2021-05-21 05:36:14 +0200

    WIP: adding create_battleship_game implementation

--- battleship/api.py
+++ battleship/api.py
@@ -1,13 +1,38 @@
 from http import HTTPStatus
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from battleship.models.game import Game
+
+from battleship.exceptions import InvalidShipsCount, ShipOverflowError, ShipOverlapingError
+
+from traceback import print_exc
 
 app = Flask(__name__)
 GAME = None
 
 @app.route('/battleship', methods=['POST'])
 def create_battleship_game():
-    return jsonify({}), HTTPStatus.NOT_IMPLEMENTED
+    body = request.json
+
+    try:
+        GAME = Game(body.get("ships"))
+
+    except InvalidShipsCount as e:
+        print_exc()
+        return jsonify({"message": e.msg}), HTTPStatus.EXPECTATION_FAILED
+
+    except ShipOverflowError as e:
+        print_exc()
+        return jsonify({"message": e.msg}), HTTPStatus.EXPECTATION_FAILED
+
+    except ShipOverlapingError as e:
+        print_exc()
+        return jsonify({"message": e.msg}), HTTPStatus.EXPECTATION_FAILED
+    except Exception as e:
+        print_exc()
+        return jsonify({"message": e.get("msg")}), HTTPStatus.INTERNAL_SERVER_ERROR
+
+    return jsonify({"message": "Game created successfully"}), HTTPStatus.OK
 
 
 @app.route('/battleship', methods=['PUT'])
