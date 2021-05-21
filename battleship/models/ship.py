010bc0c7f69c849e484299a3e41e05bc6ec9e258
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2021-05-21 05:33:22 +0200

    implementing ship model

---
+++ battleship/models/ship.py
@@ -0,0 +1,17 @@
+from .point import Point
+
+class Ship():
+    def __init__(self, point: Point, direction: str, size: int = 1) -> None:
+        self.point = point
+        self.direction = direction
+        self.size = size
+
+
+    def get_ship(self):
+        """yields a list of coordinates of this ship"""
+        if self.direction == "V":
+            for i in range(0, self.size):
+                yield (self.point.x, self.point.y + i) 
+        else:
+            for i in range(0, self.size):
+                yield (self.point.x + i, self.point.y)
